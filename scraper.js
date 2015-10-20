3ea171a30d7ee03b0a9e10d457a0db441fd409e2
Pau Torres <ptmonlinework@gmail.com>
2015-10-20 15:43:57 +0200

    almost done .... fixed possibilies

--- scraper.js
+++ scraper.js
@@ -2,36 +2,34 @@ var request = require('request');
 var cheerio = require('cheerio');
 var colors = require('colors');
 var prompt = require('prompt');
-// console.log(process.argv.length)
-// var theme = process.argv;
-// console.log(theme)
-// var theme2 = theme.slice(2, process.argv.length)
-// var theme3 = theme2.toString().replace(',', '_');
-// console.log('title is: ' + theme3);
 var albumTitle,
     albumArtist,
-    finishString;
-  var  properties=[
-      {
-        name: 'title', 
-        validator: /^[a-zA-Z\s\-]+$/,
-        warning: 'Title must be only letters, spaces, or dashes'
-      },
-      {
-        name: 'artist',
-        hidden: false
-      }
-    ]
+    finishString,
+    count = 0
+    thealbumarray = [],
+    theartistarray = [];
+
+var  properties=[
+    {
+      name: 'title', 
+      validator: /^[a-zA-Z\s\-]+$/,
+      warning: 'Title must be only letters, spaces, or dashes'
+    },
+    {
+      name: 'artist',
+      hidden: false
+    }
+  ]
   
- 
+ console.reset = function () {
+    return process.stdout.write('\033c');
+  }
+  console.reset();
   prompt.start();
- 
+
   prompt.get(properties, function (err, result) {
-    console.log(result.title.split(' ').length)
-    var thealbumarray = [];
-    var theartistarray = []
     for (var x = 0; x < result.title.split(' ').length; x++){
-      if(result.title.split(' ')[x] == 'and' || result.title.split(' ')[x] == 'of' || result.title.split(' ')[x] == 'to'){
+      if(result.title.split(' ')[x] == 'and' || result.title.split(' ')[x] == 'of' || result.title.split(' ')[x] == 'to' || result.title.split(' ')[x] == 'in'){
         thealbumarray.push(result.title.split(' ')[x].toLowerCase());
       }else{
         thealbumarray.push(result.title.split(' ')[x][0].toUpperCase() + result.title.split(' ')[x].slice(1).toLowerCase());
@@ -41,36 +39,33 @@ var albumTitle,
       theartistarray.push(result.artist.split(' ')[y][0].toUpperCase() + result.artist.split(' ')[y].slice(1).toLowerCase());
     }
     
-
-    console.log(thealbumarray);
-    console.log(theartistarray);
     titleAlbum = thealbumarray.toString().replace(/,/g , "_");
     titleArtist = theartistarray.toString().replace(/,/g, "_");
-    console.log(titleAlbum);
-    console.log(titleArtist)
-    searchItems(titleAlbum,titleArtist)
-  });
+    checkString(titleAlbum, titleArtist)
+   });
 
-function searchItems(titleAlbum,titleArtist){
-  // console.log(titleAlbum + ' ' + titleArtist)
-  
-  if(titleArtist == undefined || titleArtist == 'undefined' || titleArtist ==null ){
-    finishString = titleAlbum + '_(album)';
-  // console.log('this is undefined, artist:  ' +titleArtist )
-  
-
-  }else{
-    finishString = titleAlbum + '_(' + titleArtist +'_album)';
-   // console.log('this is NOT undefined')
-   
+  function checkString(titleAlbum, titleArtist){
+    count;
+    // if(titleArtist == undefined || titleArtist == 'undefined' || titleArtist ==null ){
+    if(count == 1){
+      finishString = titleAlbum + '_(album)';
+    }else if(count == 2){
+      finishString = titleAlbum
+    }else if(count == 3){
+      console.log(colors.red('sorry! no match found'));
+      return false;
+    }else{
+      finishString = titleAlbum + '_(' + titleArtist +'_album)';
+    }
+    searchItems(finishString)
   }
+ 
 
-  // console.log('finishString: ' + finishString)
+function searchItems(finishString){
+  console.log("searching... https://en.wikipedia.org/wiki/" + finishString)
   request({
     uri: "https://en.wikipedia.org/wiki/" + finishString,
   }, function(error, response, body) {
-    // console.log(response)
-    
     var $ = cheerio.load(body);
     var firstHeader = $('.firstHeading').text();
 
@@ -78,7 +73,8 @@ function searchItems(titleAlbum,titleArtist){
     var ititle = $('.wikitable.infobox').find('td > i > a');
     
     if($('#noarticletext').length == 1){
-      searchItems(titleAlbum)
+      count += 1;
+      checkString(titleAlbum)
     }else{
       console.log(colors.red.underline(firstHeader))
       $(title).each(function(){
